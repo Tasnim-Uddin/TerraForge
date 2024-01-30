0704592ffbf6f49497aaacd7f89a804bfd183f6c
Tasnim Uddin Syed <112864578+Tasnim-Uddin@users.noreply.github.com>
2024-01-30 21:29:35 +0000

    Delete texture_data.py

--- texture_data.py
+++
@@ -1,7 +0,0 @@
-from global_constants import *
-
-block_texture_data = {
-    "grass": {"type": "block", "size": (BLOCK_SIZE, BLOCK_SIZE), "position": (0, 0)},
-    "dirt": {"type": "block", "size": (BLOCK_SIZE, BLOCK_SIZE), "position": (0, 1)},
-    "stone": {"type": "block", "size": (BLOCK_SIZE, BLOCK_SIZE), "position": (1, 0)},
-}
