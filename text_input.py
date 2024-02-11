cd4dda3d5fbd2c69134121f621997fb85227e940
Tasnim Uddin Syed <112864578+Tasnim-Uddin@users.noreply.github.com>
2024-02-11 12:09:07 +0000

    no validation required in TextInput update method
    validation to determine current option is done in the new_player_menu_creation and new_world_menu_creation methods in the game class. so, validation to check if the text box is active is not required anymore (for colour changing).

--- text_input.py
+++ text_input.py
@@ -13,24 +13,32 @@ class TextInput:
         self.color_active = pygame.Color('dodgerblue2')
         self.font = pygame.font.Font(filename=None, size=32)
 
+    # def update(self):
+    #     for event in EventManager.events:
+    #         if event.type == pygame.MOUSEBUTTONDOWN:
+    #             if self.input_rect.collidepoint(event.pos):
+    #                 self.active = not self.active
+    #             else:
+    #                 self.active = False
+    #         if event.type == pygame.KEYDOWN:
+    #             if self.active:
+    #                 if event.key == pygame.K_RETURN:
+    #                     self.active = False
+    #                 elif event.key == pygame.K_BACKSPACE:
+    #                     self.input_text = self.input_text[:-1]
+    #                 else:
+    #                     self.input_text += event.unicode
+
     def update(self):
         for event in EventManager.events:
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.input_rect.collidepoint(event.pos):
-                    self.active = not self.active
-                else:
-                    self.active = False
             if event.type == pygame.KEYDOWN:
-                if self.active:
-                    if event.key == pygame.K_RETURN:
-                        self.active = False
-                    elif event.key == pygame.K_BACKSPACE:
-                        self.input_text = self.input_text[:-1]
-                    else:
-                        self.input_text += event.unicode
+                if event.key == pygame.K_BACKSPACE:
+                    self.input_text = self.input_text[:-1]
+                else:
+                    self.input_text += event.unicode
 
     def draw(self, screen):
-        colour = self.color_active if self.active else self.color_inactive
+        colour = self.color_active  # if self.active else self.color_inactive
         pygame.draw.rect(surface=screen, color=colour, rect=self.input_rect, width=2)
         text_surface = self.font.render(text=self.input_text, antialias=True, color=(255, 255, 255))
         screen.blit(source=text_surface, dest=(self.input_rect.x + 5, self.input_rect.y + 5))
